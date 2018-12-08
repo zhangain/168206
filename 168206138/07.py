896982a7e2db45899466ecb11135371655d75e38
zhangain <44491973+zhangain@users.noreply.github.com>
2018-12-08 11:51:44 +0800

    Update 07.py

--- 168206138/07.py
+++ 168206138/07.py
@@ -1,21 +1,39 @@
 start = 'hit'
 end = 'cog'
+adict = ['hot','dot','dog','lot','log']
 count = []
 count.append(start)
-def huang(start, end):
-	for s in range(0, int(len(start))):
-		for z in range(ord('a'), ord('z')+1):
-			start[s] = chr(z)
-			if start[s] == end[s]:
-				c = ''
-				for s in range(0, int(len(start))):
-					c += start[s]
-				count.append(c)
-				break
+def xiang(adict, a):
+	zhi = {}
+	c = []
+	for n in range(len(adict)):
+		zhi[adict[n]] = 0
+	for n in range(len(a)):
+		for m in range(len(adict)):
+			if a[n] == adict[m][n]:
+				zhi[adict[m]] += 1
+	for key,value in zhi.items():
+		if value == 2:
+			c.append(key)
+	return c
 
-astart = []
-for s in range(0, int(len(start))):
-	astart.append(start[s])	
-huang(astart, end)
-print("路径：")
-print(count)
+def zao(adict, start, end, en):	
+	st = xiang(adict, start)
+	if len(st) == 0:
+		return 
+	for n in range(len(st)):
+		if st[n] in count:
+			continue
+		count.append(st[n])
+		start = st[n]
+		if start in en:
+			count.append(end)
+			print(count)
+			count.remove(end)
+		else:
+			zao(adict, start, end, en)
+				
+
+en = xiang(adict, end)
+zao1 = zao(adict, start, end, en)
+print(zao1)
