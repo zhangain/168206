61c3b6cb17a8344d1def94a99d5fd0cbacbd1287
zhangain <44491973+zhangain@users.noreply.github.com>
2018-12-15 17:49:17 +0800

    Update 07.py
    单词长度不同也可以比较

--- 168206138/07.py
+++ 168206138/07.py
@@ -1,19 +1,20 @@
 start = 'hit'
 end = 'cog'
-adict = ['hot','dot','dog','lot','log']
+adict = ['hot', 'dot', 'dog', 'loc', 'hi', 'doge', 'lot', 'log']
 count = []
 count.append(start)
-def xiang(adict, a): #在 adict 中找与 a 相似差异为 1 个单位的
+def xiang(adict, start):#在 adict 中找与 a 相似差异为 1 个单位的
 	zhi = {}
 	c = []
 	for n in range(len(adict)):
 		zhi[adict[n]] = 0
-	for n in range(len(a)):
+	for n in range(len(start)):
 		for m in range(len(adict)):
-			if a[n] == adict[m][n]:
-				zhi[adict[m]] += 1
+			if len(adict[m]) == len(start):#单词长度一样 才有机会相似度 +1
+				if start[n] == adict[m][n]:
+					zhi[adict[m]] += 1
 	for key,value in zhi.items():
-		if value == len(a) - 1:
+		if value == len(start) - 1:
 			c.append(key)
 	return c
 
@@ -32,6 +33,9 @@ def zao(adict, start, end, en):
 		else:
 			zao(adict, start, end, en)
 			count.remove(st[n])
+				
+
 en = xiang(adict, end)
 zao1 = zao(adict, start, end, en)
 print(zao1)
+
