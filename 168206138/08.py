f9e75e7bdf707f530430546f5b6ccca0b18ee362
zhangain <44491973+zhangain@users.noreply.github.com>
2018-12-31 20:54:34 +0800

    Update 08.py

--- 168206138/08.py
+++ 168206138/08.py
@@ -5,19 +5,22 @@ C：B是盗窃这块钻石的罪犯。
 D：B有意诬陷我。
 假定只有一个人说的是真话，编程序判断谁偷走了钻石。
 """
-St = ['A','B','C','D']
+thief = ['A','B','C','D']
 
-print("小偷是：",end="")
-for s in St:
+for s in thief:
 	i = s  
 	A=(i != 'A')
 	B=(i == 'D')
 	C=(i == 'B')
 	D=(i != 'D')
-	if (A + B + C + D == 1):
+	if (A + B + C + D == 1):	
 		lists = [A,B,C,D]
-		print(i,end=" ")
-		print("\n说真话的是：",end="")
-		for i in range(len(lists)):
-			if lists[i]:
-				print(St[i],end=" ")
+		print("\n如果说真话的是：",end="")
+		for p in range(len(lists)):
+			if lists[p]:
+				print(thief[p],end=" ")
+		print("\n")
+		print("小偷是：",end="")
+		print(i)
+		
+
